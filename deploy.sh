fc56411220f328e9eb525fd08b40bf6d0b36b819
andyrohdev <rohjamesandy@gmail.com>
2025-01-28 10:47:29 -0500

    After throwing my head at the wall, the deploy script was changed it it deployed finally.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
-set -e
+#!/usr/bin/env sh
+
+
 
 echo "Building the project..."
 npm run build
@@ -9,12 +11,15 @@ cd dist
 echo "Initializing git repository..."
 git init
 
+# Switch to gh-pages branch to match the remote
+git checkout -b gh-pages
+
 echo "Adding and committing changes..."
 git add -A
 git commit -m 'New Deployment'
 
 echo "Pushing to gh-pages branch..."
-git push -f https://github.com/andyrohdev/portfolio-website.git master:gh-pages
+git push -f https://github.com/andyrohdev/portfolio-website.git gh-pages
 
 echo "Returning to the previous directory..."
 cd -
